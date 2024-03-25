09727f2ac65f96da90050007928f5a45838ed96e
Keegan Farkas <85268378+KeeganFarkas@users.noreply.github.com>
2024-03-25 13:49:34 +0000

    3/25 class

--- nodeExample/index.js
+++ nodeExample/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const { TOOBIG } = require('sqlite3');
+const sqlite3 = require('sqlite3').verbose();
+const dbSource = "node.db";
+const db = new sqlite3.Database(dbSource);
 
 const HTTP_PORT = 8000;
 console.log("Listening on port " + HTTP_PORT);
@@ -28,45 +32,61 @@ app.get("/", (req, res, next) => {
 app.post("/fruit",(req,res,next) => {
     let strName = req.query.name;
     let strColor = req.query.color;
-    arrFruit.push(new Fruit(strName,strColor));
-    res.status(201).send(arrFruit);
+    let strCommand = "INSERT INTO tblFruit VALUES(?,?)"
+    if(strName && strColor){
+        let arrParameters = [strName,strColor];
+        let objFruit = new Fruit(strName,strColor);
+        db.run(strCommand,arrParameters,function(err,result){
+            if(err){
+                res.status(400).json({error:err.message});
+            } else{
+                res.status(201).json({message:"success",fruit:objFruit})
+            }
+        })
+    } else{
+        res.status(400).json({error:"Not all parameters provided"});
+    }
 })
 
 app.get("/fruit",(req,res,next) => {
     let strName = req.query.name;
     if(strName){
-        arrFruit.forEach(function(fruit){
-            if(fruit.name == strName){
-                res.status(200).send(fruit);
+        let strCommand = "SELECT * FROM tblFruit WHERE name = ?";
+        let arrParameters = [strName];
+        db.all(strCommand,arrParameters,(err,row) => {
+            if(err){
+                res.status(400).json({error:err.message});
+            } else{
+                if(row.length < 1){
+                    res.status(200).json({message:"error: not found"});
+                } else{
+                    res.status(200).json({message:"success",fruit:row});
+                }
             }
         })
-        res.status(200).send({message:'Fruit Not Found'});
     } else{
-        res.status(200).send(arrFruit);
+        res.status(400).json({error:"No fruit name provided"});
     }
 })
 
 app.delete("/fruit",(req,res,next) => {
     let strName = req.query.name;
     if(strName){
-        arrFruit.forEach(function(fruit,index){
-            if(fruit.name == strName){
-                arrFruit.splice(index,1);
-                res.status(200).send(fruit);
-            }
-        })
-        res.status(200).send({message:'Fruit Not Found'});
+        let strCommand = "SELECT * FROM tblFruit WHERE name = " + strName;
     } else{
-        arrFruit = [];
-        res.status(200).send(arrFruit);
+        res.status(400).json({error:"No fruit name provided"});
     }
 })
 
 app.get("/hello", (req,res,next) => {
-    let strFruit = req.query.fruit;
-    console.log("Routed to hello route");
-    console.log(strFruit);
-    res.status(200).send("Hello " + strFruit);
+    let strCommand = 'SELECT * FROM tblFruit';
+    db.all(strCommand,(err,row) => {
+        if(err){
+            res.status(400).json({error:err.message});
+        } else{
+            res.status(200).json({message:"success",fruit:row});
+        }
+    })
 })
 
 app.listen(HTTP_PORT);
