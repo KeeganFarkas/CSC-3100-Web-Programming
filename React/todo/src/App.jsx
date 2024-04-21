57dde5f0e8322899cc24990d955597427b37594f
kwfarkas42 <kwfarkas42@tntech.edu>
2024-04-21 07:53:45 -0500

    4/17 class

--- React/todo/src/App.jsx
+++ React/todo/src/App.jsx
@@ -2,33 +2,37 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/sweetalert2/dist/sweetalert2.min.css'
+import SweetAlert2 from 'react-sweetalert2'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [strTaskName, setTaskName] = useState('')
+  const [swalProps, setSwalProps] = useState({})
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      {/* // create a form that takes inputs for task name, task location, due date, and a button to add the task */}
+      <div className="container">
+        <h1>Todo App</h1>
+        <form>
+          <div className="mb-3">
+            <label for="taskName" className="form-label">Task Name</label>
+            <input type="text" className="form-control" id="taskName" value={strTaskName} onChange={(e) => setTaskName(e.target.value)}/>
+          </div>
+          <div className="mb-3">
+            <label for="taskLocation" className="form-label">Task Location</label>
+            <input type="text" className="form-control" id="taskLocation" />
+          </div>
+          <div className="mb-3">
+            <label for="dueDate" className="form-label">Due Date</label>
+            <input type="date" className="form-control" id="dueDate" />
+          </div>
+          <button type="button" className="btn btn-primary" onClick={() => {setSwalProps({show:true, title:'Success', icon:'success', text:`New Task Added: ${strTaskName}`})}}>Add Task</button>
+          <SweetAlert2 {...swalProps}/>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          <span className="text-light">Edit</span> <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
